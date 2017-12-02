117962897967309b08d77cd0d5bf3bc998525427
Johann Dilantha Nallathamby <johann@Johanns-MacBook-Pro.local>
2017-12-02 18:58:43 +0530

    Committing configured files

--- 02-User-Profile/auth0-variables.js
+++ 02-User-Profile/auth0-variables.js
@@ -1,4 +1,4 @@
-var AUTH0_CLIENT_ID='{CLIENT_ID}';
-var AUTH0_DOMAIN='{DOMAIN}';
+var AUTH0_CLIENT_ID='lgecrsa5dBKroHG3O62uuz026GUC6PGx';
+var AUTH0_DOMAIN='oauth2sample.auth0.com';
 var AUTH0_CALLBACK_URL=location.href;
 
--- 04-Authorization/auth0-variables.js
+++ 04-Authorization/auth0-variables.js
@@ -1,5 +1,5 @@
-var AUTH0_CLIENT_ID = '{CLIENT_ID}';
-var AUTH0_DOMAIN = '{DOMAIN}';
-var AUTH0_AUDIENCE = '{API_IDENTIFIER}';
+var AUTH0_CLIENT_ID = 'lgecrsa5dBKroHG3O62uuz026GUC6PGx';
+var AUTH0_DOMAIN = 'oauth2sample.auth0.com';
+var AUTH0_AUDIENCE = 'https://api.mysite.com';
 var AUTH0_CALLBACK_URL = location.href;
 
